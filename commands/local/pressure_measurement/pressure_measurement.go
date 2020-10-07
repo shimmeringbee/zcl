1c3a9a92c226179d4b3817f0afc6a469e706db36
Peter Wood <peter@alastria.net>
2020-10-07 21:56:36 +0100

    Add pressure and relative humidity measurements clusters.

---
+++ commands/local/pressure_measurement/pressure_measurement.go
@@ -0,0 +1,10 @@
+package pressure_measurement
+
+import "github.com/shimmeringbee/zcl"
+
+const (
+	MeasuredValue    = zcl.AttributeID(0x0000)
+	MinMeasuredValue = zcl.AttributeID(0x0001)
+	MaxMeasuredValue = zcl.AttributeID(0x0002)
+	Tolerance        = zcl.AttributeID(0x0003)
+)
